b1d00f43ee3b6544a10bdd96ff11675e6ea59066
Kevin Hatfield <kredleader6262@gmail.com>
2023-09-02 17:17:18 -0400

    added basic file structure

--- client.py
+++ client.py
@@ -1,1 +1,26 @@
-#initialize Test
+import configparser
+import sys
+import discord
+import shutil
+
+#check for config file called settings.conf, if not, copy the file called settings.conf.example to settings.conf
+try:
+    open('settings.conf')
+except FileNotFoundError:
+    print('No config file found, copying example config file to settings.conf/nPlease edit settings.conf with your bot token and other settings')
+    try:
+        shutil.copyfile('settings.conf.example', 'settings.conf')
+    except:
+        print('Error copying example config file to settings.conf')
+        sys.exit(1)
+except:
+    print('Error opening settings.conf')
+    sys.exit(1)
+    
+#read config file
+config = configparser.ConfigParser()
+config.read('settings.conf')
+
+#for each variable in the default section in the config file, print each variable
+for key in config['DEFAULT']:
+    print(key + ': ' + config['DEFAULT'][key])
